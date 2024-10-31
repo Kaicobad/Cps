03dc53029259201da42bdc450baf574955ef03df
Kaicobad <kaikobad.kaikobad326@gmail.com>
2024-10-31 17:13:18 +0600

    init2

--- NumberLineJumps/NumberLineJumps/Program.cs
+++ NumberLineJumps/NumberLineJumps/Program.cs
@@ -30,11 +30,11 @@ namespace NumberLineJumps
         }
         public static string kangaroo(int x1, int v1, int x2, int v2)
         {
-            if ((x2 > x1 && v2 > v1) || (x1 > x2 && v1 > v2))
+            if (x2>x1 && v2>v1)
             {
                 return "NO";
             }
-            if ((x1 + v1) == (x2 + v2))
+            else if ((x2 - x1) % Math.Abs(v2 - v1) == 0)
             {
                 return "YES";
             }
